2046b2ac85a3b59052b99f85ac6ba18d6351aec9
mgamell <mgamell@laptop_asus>
2012-10-19 12:59:45 -0400

    Login window, jdbc included in lib/ directory

--- src/ads/presentation/ClientController.java
+++ src/ads/presentation/ClientController.java
@@ -21,6 +21,7 @@ public class ClientController {
     public static void main(String[] args) {
         String host = (args.length < 1) ? null : args[0];
         ClientController c = new ClientController(host);
+        
     }
 
     private ClientController(String host) {
@@ -28,16 +29,14 @@ public class ClientController {
             Registry registry = LocateRegistry.getRegistry(host);
             server = (ServerControllerInterface) registry.lookup("ServerControllerInterface");
             
-            Boolean response = true;
             try {
                 server.stopServer("mgamell", "password");
             } catch(Exception e) {}
-            System.out.println("response: " + response);
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
         }
-       
+        
     }
         
 }
