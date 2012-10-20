f8b629e03daaa11a09c79d3597cdd6761b74e079
mgamell <mgamell@laptop_asus>
2012-10-19 20:29:54 -0400

    Persistance improved, some screens added, login and register almost working (some checks in logic layer needed). Naming updated

---
+++ src/ads/resources/data/ADSUser.java
@@ -0,0 +1,121 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ads.resources.data;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author mgamell
+ */
+@Entity
+public class ADSUser implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @OneToOne(fetch=FetchType.LAZY)
+//    @JoinColumn(nullable=false)
+    private Office office;
+    private String email;
+    private String username;
+    private String password;
+
+    public ADSUser() {
+    }
+
+    public ADSUser(String firstName, String lastName, Office office, String email, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.office = office;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ADSUser)) {
+            return false;
+        }
+        ADSUser other = (ADSUser) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ads.data.User[ id=" + id + " ]";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
